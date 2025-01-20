d13a3ae29f211b542e114fd4cfc387fed715bc6c
lily313999 <lily5203139@gmail.com>
2025-01-20 14:36:55 +0800

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateImagePaths(folder, prefix, count) {
 }
 
 // 生成紅包圖片
-const rewards = generateImagePaths('a02', 'reward', 14);
+const rewards = generateImagePaths('a02', 'reward', 23);
 
 
 // 特定暱稱的紅包內容（喵喵固定的圖片）
